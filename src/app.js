bf2157d84d0943fff47fb2053712a4a23ecaa203
abizmo <abizmo@gmail.com>
2019-01-08 17:30:51 +0000

    Parse json

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(helmet({
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "..", "dist")));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
