e1e8acdbebbf084e231d56a3ebc68b9fdb1c21d2
abizmo <abizmo@gmail.com>
2019-01-10 18:35:18 +0000

    Redux_Devtools_extension config

--- src/public/index.jsx
+++ src/public/index.jsx
@@ -3,10 +3,13 @@ const ReactDOM = require('react-dom');
 const { Provider } = require('react-redux');
 const { createStore } = require('redux');
 const App = require('./components/App/App');
-const reducer = require('./reducers');
+const reducers = require('./reducers');
 
 const target = document.getElementById('app');
-const store = createStore(reducer);
+const store = createStore(
+  reducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={ store }>
